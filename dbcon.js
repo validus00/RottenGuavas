2bcc20739e75012f244cfb1bfff4b50729f283a0
Validus <validus00@gmail.com>
2020-05-15 17:46:27 -0700

    minor changes

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : ' ',
+  password        : ' ',
+  database        : ' '
 });
 module.exports.pool = pool;
