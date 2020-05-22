283c4ec4156b3a305a01907ad83e8cafb6d2886b
validus00 <47204259+validus00@users.noreply.github.com>
2020-05-22 14:12:52 -0700

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_castanap',
-  password        : '8Hp59Pd@$v',
-  database        : 'cs340_castanap'
-});
-module.exports.pool = pool;
