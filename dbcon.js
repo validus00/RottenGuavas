0dca52d7bf01f54e77fab24f15eb23c786f1717c
April Castaneda <castanap@oregonstate.edu>
2020-05-22 14:08:39 -0700

    AddGame and Register capabilities

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : ' ',
-  password        : ' ',
-  database        : ' '
+  user            : 'cs340_castanap',
+  password        : '8Hp59Pd@$v',
+  database        : 'cs340_castanap'
 });
 module.exports.pool = pool;
